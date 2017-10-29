4677b7065b4dd7d30915c42b9830487a00072197
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-29 13:48:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,38 @@ function setCart(c) {
   return cart;
 }
 
+
+
 function addToCart(item) {
  // write your code here
+ var price =Math.floor(Math.random()*100)
+ var object = {[item]:price}
+ cart.push(object)
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
+
 
 function viewCart() {
   // write your code here
+  var name=""
+  var i= 0
+
+  if(cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  }else{
+    while(i < cart.length -2) {
+      var items = Object.keys(cart[i])[0]
+      name =  name + items + " at $" + var object[items] + ",";
+      i++
+    }
+    var items= Object.keys(cart[i])[0]
+    name =  name + items + " at $" + var object[items] + ".";
+  }
+console.log("In your cart, you have " + name);
+
 }
+
 
 function total() {
   // write your code here
