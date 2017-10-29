6778a1836b2e962ef6c17f359be75747605a1924
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-29 14:06:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function viewCart(){
     for(var i=0; i< cart.length ;i++){
       console.log(cart[i])
       var object = cart[i]
-console.log(object.value)
+console.log(object.item)
       cartItems.push(Object.keys(cart[i])[0] + " at " + object.value)
     }
   }
