4f1760a1c3e4605777f5bf7dac42c74153907e19
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-29 13:55:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart(){
   if(cart.length===0){
     console.log("Your shopping cart is empty.")
   }else{
-    var cartItems[];
+    var cartItems=[];
     for(var i=0; i<cart.length);i++){
       console.log(cart[i])
       cartItems.push(cart[i])
