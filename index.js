30beab2a03e67e0ba5238324d42ea59bce2076e3
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-29 15:40:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function placeOrder(cardNumber) {
   if(cardNumber===undefined){
     console.log("Sorry, we don't have a credit card on file for you.")
   }else {
-    var total = total()
-    console.log(`Your total cost is ${total}, which will be charged to the card ${cardNumber}.`)
+    var totalPurchase = total()
+    console.log(`Your total cost is ${totalPurchase}, which will be charged to the card ${cardNumber}.`)
   }
 }
